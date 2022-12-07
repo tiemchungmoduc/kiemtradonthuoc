6b9fcccd34ce7d19f37f7b351824064b1627817c
tiemchungmoduc <112737604+tiemchungmoduc@users.noreply.github.com>
2022-12-07 15:59:55 +0700

    Update dom_source_select2_example.js

--- resources/js/dom_source_select2_example.js
+++ resources/js/dom_source_select2_example.js
@@ -11,7 +11,7 @@ $(document).ready(function () {
 		"bStateSave": true
 	}).yadcf([
 	    {column_number : 0, filter_type: "multi_select", select_type: 'select2'},
-	    {column_number : 1, filter_type: "range_number_slider"},
+	    {column_number : 1, filter_type: "multi_select", select_type: 'select2'},
 	    {column_number : 2, select_type: 'select2', select_type_options: {width: '150px', minimumResultsForSearch: -1}},
 	    {column_number : 3, filter_type: "text", text_data_delimiter: ",", exclude: true},
 	    {column_number : 4, select_type: 'select2', select_type_options : {width: '150px', placeholder: 'Select tag', allowClear: true}, column_data_type: "html", html_data_type: "text", filter_reset_button_text: false}]);
