64ac74b95eae2c4bd9a060f2a2a6645ba2f85715
tiemchungmoduc <112737604+tiemchungmoduc@users.noreply.github.com>
2022-12-08 14:19:33 +0700

    Update dom_source_select2_example.js

--- resources/js/dom_source_select2_example.js
+++ resources/js/dom_source_select2_example.js
@@ -1,32 +1,48 @@
 /*global $, SyntaxHighlighter*/
 
-var oTable;
 
+var oTable2;
 
 $(document).ready(function () {
-	'use strict';
+    'use strict';
 
-	oTable = $('#example').dataTable({
-		"responsive": true,
-					        "processing": true,
-					        "ajax": "/kiemtradonthuoc/resources/Deep.txt",
-					        "columns": [{
-					            "data": "engine"
-					        }, {
-					            "data": "browser"
-					        }, {
-					            "data": "platform.inner"
-					        }, {
-					            "data": "platform.details.0"
-					        }, {
-					            "data": "platform.details.1"
-					        }]
-	}).yadcf([
-	    {column_number : 0, filter_type: "multi_select", select_type: 'select2'},
-	    {column_number : 1, filter_type: "multi_select", select_type: 'select2'},
-	    {column_number : 2, select_type: 'select2', select_type_options: {width: '150px', minimumResultsForSearch: -1}},
-	    {column_number : 3, filter_type: "text", text_data_delimiter: ",", exclude: true},
-	    {column_number : 4, select_type: 'select2', select_type_options : {width: '150px', placeholder: 'Select tag', allowClear: true}, column_data_type: "html", html_data_type: "text", filter_reset_button_text: false}]);
+    oTable2 = $('#example').DataTable({
+    	"responsive": true,
+        "processing": true,
+        "ajax": "/kiemtradonthuoc/resources/Deep.txt",
+        "columns": [{
+            "data": "engine"
+        }, {
+            "data": "browser"
+        }, {
+            "data": "platform.inner"
+        }, {
+            "data": "platform.details.0"
+        }, {
+            "data": "platform.details.1"
+        }]
+    });
 
-	SyntaxHighlighter.all();
+    yadcf.init(oTable2, [{
+        column_number: 0
+    }, {
+        column_number: 1,
+        filter_type: "text",
+        exclude: true,
+        exclude_label: '!(not)'
+    }, {
+        column_number: 2,
+        filter_type: "auto_complete"
+    }, {
+        column_number: 3,
+        filter_type: "range_number_slider",
+        ignore_char: "-"
+    }, {
+        column_number: 4
+    }]);
+    
+	yadcf.exFilterColumn(oTable2, [[0, "Misc"]]);
+	
+    SyntaxHighlighter.all();
+
 });
