8e069b7cafb864a165b2be19a9f8a5640d8cc05d
tiemchungmoduc <112737604+tiemchungmoduc@users.noreply.github.com>
2022-12-09 13:37:35 +0700

    Create demo.js

---
+++ resources/js/demo.js
@@ -0,0 +1,66 @@
+/*global $, SyntaxHighlighter*/
+
+
+var oTable,
+	oTable2;
+
+$(document).ready(function () {
+    'use strict';
+
+    
+    oTable = $('#example').DataTable({
+		//stateSave: true,
+		fixedHeader: true
+    });
+    
+    
+
+    oTable2 = $('#entrys_table').DataTable({
+    	"responsive": true,
+        "processing": true,
+        "ajax": "resources/deep.txt",
+        "columns": [{
+            "data": "engine"
+        }, {
+            "data": "browser"
+        }, {
+            "data": "platform.inner"
+        }, {
+            "data": "platform.details.0"
+        }, {
+            "data": "platform.details.1"
+        }]
+    });
+
+    yadcf.init(oTable2, [{
+        column_number: 0
+    }, {
+        column_number: 1,
+        filter_type: "text",
+        exclude: true,
+        exclude_label: '!(not)'
+    }, {
+        column_number: 2,
+        filter_type: "multi_select", 
+	select_type: 'select2'
+    }, {
+        column_number: 3,
+        filter_type: "range_number_slider",
+        ignore_char: "-"
+    }, {
+        column_number: 4
+    }]);
+    
+	yadcf.exFilterColumn(oTable2, [[0, "Misc"]]);
+	
+	yadcf.initMultipleColumns([oTable], [{
+		column_number: [0, 1],
+		filter_container_id: 'multi-table-filter', 
+		filter_default_label: 'Filter all tables!',
+		filter_match_mode : "contains",
+		filter_type: 'multi_select',
+        	select_type: 'select2'
+	}]);
+    SyntaxHighlighter.all();
+
+});
