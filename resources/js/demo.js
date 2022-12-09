fecd32cf09a08f9f947cf94397ddbc9ef863b4af
tiemchungmoduc <112737604+tiemchungmoduc@users.noreply.github.com>
2022-12-09 14:45:52 +0700

    Update demo.js

--- resources/js/demo.js
+++ resources/js/demo.js
@@ -12,47 +12,5 @@ $(document).ready(function () {
 		filter_type: 'multi_select',
         	select_type: 'select2'
 	}]);
-    
-
-    oTable2 = $('#entrys_table').DataTable({
-    	"responsive": true,
-        "processing": true,
-        "ajax": "resources/deep.txt",
-        "columns": [{
-            "data": "engine"
-        }, {
-            "data": "browser"
-        }, {
-            "data": "platform.inner"
-        }, {
-            "data": "platform.details.0"
-        }, {
-            "data": "platform.details.1"
-        }]
-    });
-
-    yadcf.init(oTable2, [{
-        column_number: 0
-    }, {
-        column_number: 1,
-        filter_type: "text",
-        exclude: true,
-        exclude_label: '!(not)'
-    }, {
-        column_number: 2,
-        filter_type: "multi_select", 
-	select_type: 'select2'
-    }, {
-        column_number: 3,
-        filter_type: "range_number_slider",
-        ignore_char: "-"
-    }, {
-        column_number: 4
-    }]);
-    
-	yadcf.exFilterColumn(oTable2, [[0, "Misc"]]);
-	
-	
-    SyntaxHighlighter.all();
 
 });
