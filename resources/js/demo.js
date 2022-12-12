c027195e8829e5ed336c5e8da2d4e5ec6b3b951a
tiemchungmoduc <112737604+tiemchungmoduc@users.noreply.github.com>
2022-12-12 16:04:18 +0700

    Update demo.js

--- resources/js/demo.js
+++ resources/js/demo.js
@@ -1,12 +1,12 @@
 /*global $, SyntaxHighlighter*/
 
 
-var oTable2;
+var oTable;
 
 $(document).ready(function () {
     'use strict';
 
-    oTable2 = $('#entrys_table').DataTable({
+    oTable = $('#entrys_table').DataTable({
     	"responsive": true,
         "processing": true,
         "ajax": "resources/deep.txt",
@@ -23,30 +23,11 @@ $(document).ready(function () {
         }]
     });
 
-    yadcf.init(oTable2, [{
-        column_number: 0
-    }, {
-        column_number: 1,
-        filter_type: "text",
-        exclude: true,
-        exclude_label: '!(not)'
-    }, {
-        column_number: 2,
-        filter_type: "multi_select", 
-	select_type: 'select2'
-    }, {
-        column_number: 3,
-        filter_type: "range_number_slider",
-        ignore_char: "-"
-    }, {
-        column_number: 4
-    }]);
-    
 	
-        yadcf.initMultipleColumns(oTable2, [{
+        yadcf.initMultipleColumns(oTable, [{
 		column_number: [0, 1], 
-		filter_container_id: 'multi-column-filter-01', 
-		filter_default_label: 'First table columns 1 and 2!',
+		filter_container_id: 'kiem-tra-don-thuoc', 
+		filter_default_label: 'Nhập tên thuốc',
 		filter_match_mode : "contains",
 		filter_type: 'multi_select',
         	select_type: 'select2'
