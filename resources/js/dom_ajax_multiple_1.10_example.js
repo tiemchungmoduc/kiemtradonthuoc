51138816047ec479788802d9e5c6aa59221d2d7a
tiemchungmoduc <112737604+tiemchungmoduc@users.noreply.github.com>
2022-12-13 15:36:46 +0700

    Update dom_ajax_multiple_1.10_example.js

--- resources/js/dom_ajax_multiple_1.10_example.js
+++ resources/js/dom_ajax_multiple_1.10_example.js
@@ -1,50 +1,11 @@
 /*global $, SyntaxHighlighter*/
 
 
-var oTable,
-	oTable2;
+var oTable2;
 
 $(document).ready(function () {
     'use strict';
 
-    
-    oTable = $('#example').DataTable({
-		//stateSave: true,
-		fixedHeader: true,
-	    "responsive": true,
-        "processing": true,
-        "ajax": "resources/dit.txt",
-        "columns": [{
-            "data": "engine"
-        }, {
-            "data": "browser"
-        }, {
-            "data": "platform.inner"
-        }, {
-            "data": "platform.details.0"
-        }, {
-            "data": "platform.details.1"
-        }]
-    });
-    
-    yadcf.init(oTable, [{
-        column_number: 0
-    }, {
-        column_number: 1,
-        filter_type: "text",
-        exclude: true,
-        exclude_label: '!(not)'
-    }, {
-        column_number: 2,
-        filter_type: "multi_select", 
-	select_type: 'select2'
-    }, {
-        column_number: 3,
-        filter_type: "range_number_slider",
-        ignore_char: "-"
-    }, {
-        column_number: 4
-    }]);
     oTable2 = $('#entrys_table').DataTable({
     	"responsive": true,
         "processing": true,
@@ -64,30 +25,8 @@ $(document).ready(function () {
         }]
     });
 
-    yadcf.init(oTable2, [{
-        column_number: 0
-    }, {
-        column_number: 1,
-        filter_type: "multi_select", 
-	select_type: 'select2'
-    }, {
-        column_number: 2,
-        filter_type: "multi_select", 
-	select_type: 'select2'
-    }, {
-        column_number: 3,
-        filter_type: "select", 
-	select_type: 'select2'
-    }, {
-        column_number: 4
-    }, {
-        column_number: 5
-    }
-			]);
-    
-	
 	yadcf.initMultipleColumns(oTable2, [{
-		column_number: [0, 1], 
+		column_number: [1, 2], 
 		filter_container_id: 'multi-column-filter-01', 
 		filter_default_label: 'First table columns 1 and 2!',
 		filter_match_mode : "contains",
