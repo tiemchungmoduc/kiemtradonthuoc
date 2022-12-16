656d93004e2898a872e32a9f2b6ff15b80c0f568
tiemchungmoduc <112737604+tiemchungmoduc@users.noreply.github.com>
2022-12-16 08:10:01 +0700

    Update demo.js

--- resources/js/demo.js
+++ resources/js/demo.js
@@ -1,39 +1,50 @@
 /*global $, SyntaxHighlighter*/
 
 
-var oTable;
+var oTable2;
 
 $(document).ready(function () {
     'use strict';
-
-    oTable = $('#entrys_table').DataTable({
-    	"responsive": true,
-        "processing": true,
-        "ajax": "resources/deep.txt",
-        "columns": [{
-            "data": "STT"
-        }, {
-            "data": "Hoạt chất 1"
-        }, {
-            "data": "Hoạt chất 2"
-        }, {
-            "data": "Cơ chế"
-        }, {
-            "data": "Hậu quả"
-        }, {
-            "data": "Xử trí"
-        }]
+    oTable2 = $('#entrys_table').DataTable({
+    	//stateSave: true,
+		fixedHeader: true,
+	    "responsive": true,
     });
-
-	
-        yadcf.initMultipleColumns(oTable, [{
+	yadcf.initMultipleColumns(oTable2, [{
 		column_number: [1, 2], 
-		filter_container_id: 'kiem-tra-don-thuoc', 
-		filter_default_label: 'Nhập tên thuốc',
+		filter_container_id: 'multi-column-filter-01', 
+		filter_default_label: 'Nhập tên hoạt chất',
 		filter_match_mode : "contains",
 		filter_type: 'multi_select',
         	select_type: 'select2'
 	}]);
     SyntaxHighlighter.all();
+ var dataSrc = [];
 
+   var table = $('#entrys_table').DataTable({
+      'initComplete': function(){
+         var api = this.api();
+
+         // Populate a dataset for autocomplete functionality
+         // using data from first, second and third columns
+         api.cells('tr', [1, 2]).every(function(){
+            // Get cell data as plain text
+            var data = $('<div>').html(this.data()).text();           
+            if(dataSrc.indexOf(data) === -1){ dataSrc.push(data); }
+         });
+         
+         // Sort dataset alphabetically
+         dataSrc.sort();
+        
+         // Initialize Typeahead plug-in
+         $('.dataTables_filter input[type="search"]', api.table().container())
+            .typeahead({
+               source: dataSrc,
+               afterSelect: function(value){
+                  api.search(value).draw();
+               }
+            }
+         );
+      }
+   });
 });
