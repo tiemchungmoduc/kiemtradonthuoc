dd5952b577a8d5df65805856ffcdb7a53b079624
tiemchungmoduc <112737604+tiemchungmoduc@users.noreply.github.com>
2022-12-16 14:50:41 +0700

    Update demo.js

--- resources/js/demo.js
+++ resources/js/demo.js
@@ -9,6 +9,7 @@ $(document).ready(function () {
     	//stateSave: true,
 		fixedHeader: true,
 	    "responsive": true,
+	     ordering: false
     });
 	yadcf.initMultipleColumns(oTable2, [{
 		column_number: [1, 2], 
