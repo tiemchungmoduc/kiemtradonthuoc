3467e543e7eb41e95e21101578e8c2fb4611770c
tiemchungmoduc <112737604+tiemchungmoduc@users.noreply.github.com>
2022-12-22 14:00:16 +0700

    Update dom_ajax_multiple_1.10_example.js

--- resources/js/dom_ajax_multiple_1.10_example.js
+++ resources/js/dom_ajax_multiple_1.10_example.js
@@ -27,16 +27,12 @@ $(document).ready(function () {
         	select_type: 'chosen'
 	}]);
 	yadcf.initMultipleColumns(oTable2, [{
-		column_number: 1 , 
+		column_number: [1] , 
 		filter_container_id: 'multi-column-filter-02', 
 		filter_default_label: 'Nhập tên hoạt chất',
 		filter_match_mode : "contains",
 		filter_type: 'select',
-		select_type: 'chosen',
-		select_type_options: {
-            no_results_text: 'Can\'t find ->',
-            search_contains: true
-        }
+		select_type: 'chosen'
 	}]);
     SyntaxHighlighter.all();
 
