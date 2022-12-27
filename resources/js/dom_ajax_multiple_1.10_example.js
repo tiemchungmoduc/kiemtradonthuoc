1d0c0e8b1d958ed0d3def7a219d3c7e672bd25a4
tiemchungmoduc <112737604+tiemchungmoduc@users.noreply.github.com>
2022-12-27 09:51:01 +0700

    Update dom_ajax_multiple_1.10_example.js

--- resources/js/dom_ajax_multiple_1.10_example.js
+++ resources/js/dom_ajax_multiple_1.10_example.js
@@ -43,8 +43,8 @@ $(document).ready(function () {
          //dataSrc.sort();
 	     var opt1 = oTable2.column(1).data();
  	     var opt2 = oTable2.column(2).data();
-	     var aloha = Object.values(opt1).slice(0, 101);
-	     var alohe = Object.values(opt2).slice(0, 101);
+	     var aloha = Object.values(opt1).slice(0, 301);
+	     var alohe = Object.values(opt2).slice(0, 301);
 	     var olala = [...aloha, ...alohe];
 	     var olili = [...new Set(olala)];
         
@@ -52,7 +52,7 @@ $(document).ready(function () {
          yadcf.initMultipleColumns(oTable2, [{
    		data: olili,
     		filter_container_id: 'multi-column-filter-03', 
-	  	filter_type: "select", 
+	  	filter_type: "multi_select", 
 	  	select_type: "chosen", 
 	  	filter_default_label: "Nhập tên hoạt chất",
     		select_type_options: {
